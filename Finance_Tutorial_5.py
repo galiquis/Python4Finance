71895c0fe8d7c9dfded07b6153e2059e93a67277
Galiquis <galiquis@gmail.com>
2020-05-31 22:19:21 +0100

    Completed Tutorial 5

---
+++ Finance_Tutorial_5.py
@@ -0,0 +1,21 @@
+import bs4 as bs
+import pickle
+import requests
+
+def save_FTSE100_tickers():
+    ## Define the page we want to scrape
+    resp = requests.get('https://en.wikipedia.org/wiki/FTSE_100_Index')
+    ## use BS to pull the source
+    soup = bs.BeautifulSoup(resp.text, 'lxml')
+    table = soup.find('table',{'id':'constituents'}).find('tbody')
+    tickers = []
+    for rows in table.find_all('tr')[1:]:
+        ticker = rows.find_all('td')
+        tickers.append(ticker[1].text)
+    with open('FTSE100_Tickers.pickle', 'wb') as f:
+        pickle.dump(tickers, f)
+    print(tickers)
+    return tickers
+
+
+save_FTSE100_tickers()
